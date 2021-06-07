bb8aa6bc8972eb8a72a97da2178d5c489fe8b818
Daniel Guerrero <daniel.622.guerrero@gmail.com>
2021-06-06 23:39:59 -0300

    individual pages with faq as a collapse element

---
+++ src/pages/Divorcio/index.js
@@ -0,0 +1,35 @@
+import { lazy } from "react";
+import FAQCollapse from "../../components/FAQCollapse";
+
+import DivorcioContent from "../../content/DivorcioContent.json";
+import ContactContent from "../../content/ContactContent.json";
+
+const ContactFrom = lazy(() => import("../../components/ContactForm"));
+const ContentBlock = lazy(() => import("../../components/ContentBlock"));
+const Container = lazy(() => import("../../common/Container"));
+const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
+
+const Divorcio = () => {
+  return (
+    <Container>
+      <ScrollToTop />
+      <ContentBlock
+        type="right"
+        title={DivorcioContent.title}
+        content={DivorcioContent.content}
+        icon="product-launch.svg"
+        id="job_law"
+      />
+
+      <FAQCollapse data={DivorcioContent}/>
+    
+      <ContactFrom
+        title={ContactContent.title}
+        content={ContactContent.text}
+        id="contact"
+      />
+    </Container>
+  );
+};
+
+export default Divorcio;
