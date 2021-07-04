54b2f67500d6788a7e6d3418fac683d1a08eab83
Mvitelli <movitelli@gmail.com>
2021-07-04 20:34:50 -0300

    Removing unused buttons, fixing redirects

--- src/components/TopicBlock/index.js
+++ src/components/TopicBlock/index.js
@@ -1,31 +1,35 @@
 import React from 'react'
-import { Tabs } from 'antd';
+import { Tabs, Tooltip } from 'antd';
 import ContentBlock from '../ContentBlock';
 
 const { TabPane } = Tabs;
 
 const TopicContainer = (props) => {
   const { content } = props;
+
+  const text = <span>Hacer clic para cambiar de pestaña.</span>
   return (
     <>
-      <Tabs centered size="large" style={{ padding: "2rem 0 2rem" }}>
-        {content.map((tab, index) => {
-          return (
-            <TabPane tab={<span style={{ fontSize: "x-large" }}>{tab.title}</span>} key={index}>
-              <ContentBlock
-                type={tab.type}
-                title={tab.title}
-                content={tab.text}
-                section={tab.section}
-                icon={tab.icon}
-                id={tab.id}
-                {...tab}
-                style={{ "padding": "2rem 0 4rem" }}
-              />
-            </TabPane>
-          )
-        })}
-      </Tabs>
+      <Tooltip title={text} color={'blue'}>
+        <Tabs centered size="large" style={{ padding: "2rem 0 2rem" }}>
+          {content.map((tab, index) => {
+            return (
+              <TabPane tab={<span style={{ fontSize: "x-large" }}>{tab.title}</span>} key={index}>
+                <ContentBlock
+                  type={tab.type}
+                  title={tab.title}
+                  content={tab.text}
+                  section={tab.section}
+                  icon={tab.icon}
+                  id={tab.id}
+                  {...tab}
+                  style={{ "padding": "2rem 0 4rem" }}
+                />
+              </TabPane>
+            )
+          })}
+        </Tabs>
+      </Tooltip>
     </>
   )
 }
